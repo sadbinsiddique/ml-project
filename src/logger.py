e0b4df9d471a625193a096533ed26e09fa76ce6f
sadbinsiddique <21-45005-2@student.aiub.edu>
2025-12-22 00:11:19 +0600

    refactor logging setup to use current working directory for log file path

--- src/logger.py
+++ src/logger.py
@@ -3,14 +3,16 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d')}.log"
-LOG_PATH = os.path.join(os.getcwd(),"logs", LOG_FILE)
+LOG_PATH = os.path.join("logs", LOG_FILE)
 
 os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(LOG_PATH, LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    filename=LOG_PATH,
     format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+if __name__ == "__main__":
+    logging.info("Logging is set up.")

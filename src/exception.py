4fc0f053c3b176ca5238fb8076119c9b53267711
sadbinsiddique <21-45005-2@student.aiub.edu>
2025-12-21 23:52:00 +0600

    add __str__ method to CustomException for improved error message representation

--- src/exception.py
+++ src/exception.py
@@ -15,3 +15,6 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail: ModuleType):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
